da7f17a281384faa87142a7d4c7272b722f9927b
Abdellah Hariti <haritiabdellah@gmail.com>
2022-06-06 12:34:50 +0100

    suppress missing arg warning

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -49,7 +49,7 @@ return require('packer').startup(function(use)
   use({
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-    config = require('lualine').setup(),
+    config = require('lualine').setup({}),
   })
   use({
     'folke/which-key.nvim',
