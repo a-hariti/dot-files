48388ede8b33273c8bb405a068fcc3f4388a9087
Abdellah Hariti <haritiabdellah@gmail.com>
2022-11-20 17:47:45 +0100

    upgrade from deprecated function

--- nvim/lua/lsp-config.lua
+++ nvim/lua/lsp-config.lua
@@ -1,7 +1,9 @@
 local lsp_installer = require('nvim-lsp-installer')
-local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 local mappings = require('mappings')
 local nvim_lsp = require('lspconfig')
+
+Clients = vim.lsp.get_active_clients()
 
 lsp_installer.on_server_ready(function(server)
   local opts = {
