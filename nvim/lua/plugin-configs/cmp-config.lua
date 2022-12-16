b8c7b442ac9f7abec16e4759403afa9e77aa0b5f
Abdellah Hariti <haritiabdellah@gmail.com>
2022-12-16 17:19:28 +0100

    add friendly-snippets

--- nvim/lua/plugin-configs/cmp-config.lua
+++ nvim/lua/plugin-configs/cmp-config.lua
@@ -4,7 +4,8 @@ if not ok then
   return
 end
 local luasnip = require('luasnip')
-
+-- load vsocdoe like snippets (makes friendly-snippets work)
+require('luasnip/loaders/from_vscode').lazy_load()
 local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
