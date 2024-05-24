9765ee1a5e4e03844160b7ae760faf6e0cae1d38
Abdellah Hariti <haritiabdellah@gmail.com>
2024-05-25 00:37:09 +0100

    use nightfly colorscheme

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -1,3 +1,3 @@
-vim.cmd.colorscheme('vim-material')
+vim.cmd.colorscheme('nightfly')
 -- change the default error virtual text color
 vim.cmd([[ hi link DiagnosticVirtualTextError DiagnosticError ]])
