d3a3c43bf20e0d818c7ee2709497ed856a595c41
Abdellah Hariti <haritiabdellah@gmail.com>
2026-01-30 13:43:23 +0100

    feat(colors): switch to gruvbox-material colorscheme

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -1,3 +1,7 @@
-vim.cmd.colorscheme('gruvbox')
+vim.g.gruvbox_material_better_performance = 1
+vim.g.gruvbox_material_background = 'medium'
+vim.g.gruvbox_material_foreground = 'original'
+vim.cmd.colorscheme('gruvbox-material')
+
 -- change the default error virtual text color
 vim.cmd([[ hi link DiagnosticVirtualTextError DiagnosticError ]])
