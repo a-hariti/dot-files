75218b59ab6204e550c8ff90a8f84b925d96fc59
Abdellah Hariti <haritiabdellah@gmail.com>
2026-02-12 22:57:13 +0100

    nvim: Y to yank to system clipboard

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -45,8 +45,6 @@ map('n', '<M-down>', ':resize -1<CR>', { silent = true })
 map('n', '<M-left>', ':vertical resize -1<CR>', { silent = true })
 map('n', '<M-right>', ':vertical resize +1<CR>', { silent = true })
 
-map('n', '<leader>y', '"+y')
-map('v', '<leader>y', '"+y')
 map({ 'n', 'v' }, '<leader>p', '"+p')
 
 -- escape terminal mode
