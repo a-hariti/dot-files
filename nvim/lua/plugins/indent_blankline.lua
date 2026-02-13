eecf996b57f5056165feec27ae18d98918cfa232
Abdellah Hariti <haritiabdellah@gmail.com>
2026-02-13 19:21:34 +0100

    nvim: delete indent-blankline plugin

--- nvim/lua/plugins/indent_blankline.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'lukas-reineke/indent-blankline.nvim',
-  main = 'ibl',
-  event = { 'BufReadPost', 'BufNewFile' },
-  opts = {
-    indent = { char = '┊' },
-    scope = { show_start = false, show_end = false },
-  },
-}
