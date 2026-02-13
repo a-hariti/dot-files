33ef32babe9c1791227e9227b848a14ec8a55c0b
Abdellah Hariti <haritiabdellah@gmail.com>
2026-02-13 19:42:22 +0100

    nvim: do not choke on jsonc

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -18,3 +18,17 @@ vim.cmd([[
     au InsertEnter * set norelativenumber
     au InsertLeave * set relativenumber
 ]])
+
+local function looks_like_jsonc(bufnr)
+  local max_lines = math.min(vim.api.nvim_buf_line_count(bufnr), 200)
+  local lines = vim.api.nvim_buf_get_lines(bufnr, 0, max_lines, false)
+  local text = table.concat(lines, '\n')
+  return text:find('//', 1, true) ~= nil or text:find('/%*') ~= nil or text:find(',%s*[}%]]') ~= nil
+end
+
+vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+  pattern = '*.json',
+  callback = function(args)
+    if looks_like_jsonc(args.buf) then vim.bo[args.buf].filetype = 'jsonc' end
+  end,
+})
--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -1,16 +1,33 @@
 return {
   'nvimtools/none-ls.nvim',
-  event = { 'BufReadPre', 'BufNewFile' },
+  event = { 'BufReadPost', 'BufNewFile' },
   dependencies = { 'nvim-lua/plenary.nvim' },
   config = function()
     local null_ls = require('null-ls')
     local formatting = null_ls.builtins.formatting
-
     local function is_none_ls(client) return client.name == 'null-ls' or client.name == 'none-ls' end
 
+    local function should_attach(bufnr)
+      if vim.api.nvim_buf_line_count(bufnr) > 10000 then return false end
+      local name = vim.api.nvim_buf_get_name(bufnr)
+      if name:find('/node_modules/', 1, true) or name:find('/dist/', 1, true) then return false end
+      if name:match('%.min%.[cm]?js$') then return false end
+      if name:match('[/\\]package%-lock%.json$') then return false end
+      if name:match('[/\\]pnpm%-lock%.yaml$') then return false end
+      if name:match('[/\\]yarn%.lock$') then return false end
+      return true
+    end
+
+    local prettierd_filetypes = vim.tbl_filter(
+      function(ft) return ft ~= 'json' and ft ~= 'jsonc' end,
+      formatting.prettierd.filetypes or {}
+    )
+
     null_ls.setup({
+      should_attach = should_attach,
       sources = {
-        formatting.prettierd,
+        formatting.prettierd.with({ filetypes = prettierd_filetypes }),
+        formatting.prettier.with({ filetypes = { 'json', 'jsonc' } }),
         formatting.shfmt,
         formatting.stylua,
       },
